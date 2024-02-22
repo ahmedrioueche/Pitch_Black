77ee5aafc9bf7f20f1e6ace9828166a61d3ff450
Ahmed Drioueche <adsrahmed@gmail.com>
2024-02-22 00:47:31 +0000

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,97 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
+import { useState } from 'react'
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider, 
+  Route,
+  Link,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
 import Login from './pages/login/Login'
 import Register from './pages/register/Register';
 import Home from './pages/home/Home';
+import MyNavBar from './components/NavBar'
+import Profile from './pages/profile/Profile';
+import Studio from './pages/studio/Studio'
+import Stream from './pages/stream/Stream'
+import LeftBar from './components/LeftBar';
+
 function App() {
+
+  const [currentUser, setCurrentUser] = useState(true);
+  const ProtectedRoutes = ({children}) => {
+    if(!currentUser)
+      return <Navigate to="/login"/>;
+
+    return children;
+  } 
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: 
+        <ProtectedRoutes>
+          <Layout/>
+        </ProtectedRoutes>, 
+      children: [
+        {
+          path:"/",
+          element: <Home/>,
+        }, 
+        {
+          path:"/profile",
+          element: <Profile/>,
+        }, 
+        {
+          path:"/studio",
+          element: <Studio/>,
+        }, 
+        {
+          path:"/stream",
+          element: <Stream/>,
+        }, 
+      ],
+    },
+
+    {
+      path: "/login",
+      element: <Login/>,
+    },
+    {
+      path: "/register",
+      element: <Register/>,
+    },
+    {
+      path: "/profile",
+      element: <Profile/>,
+    },
+    {
+      path:"/studio",
+      element: <Studio/>,
+    }, 
+    {
+      path:"/stream",
+      element: <Stream/>,
+    }, 
+  ]);
+
+  function Layout() {
+    return (
+      <>
+        <MyNavBar/>
+        <div>
+          <LeftBar/>
+        </div>
+        <Outlet/>
+      </>
+    );
+  }
 
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" Component={Home}></Route>
-          <Route path="/register" Component={Register}></Route>
-          <Route path="/login" Component={Login}></Route>
-        </Routes>
-         
-      </Router>
+      <RouterProvider router={router}/>
     </>
   )
 }
